4c14bbb5ff5a0a5c8abc4af2850909fc5ca30c18
Amit78744 <amitambaliya5@gmail.com>
2021-10-03 17:13:39 +0530

    on selected plan created for user according to choose plan, subscription activated api

--- routes/test.js
+++ routes/test.js
@@ -4,9 +4,7 @@ var con = require('./database');
 
 exports.testing = (req,res) =>{
 
-    console.log("coming");
-
-    con.connect((err) => {
+    /*con.connect((err) => {
       if(!err)
           res.send('DB Connection Succeded');
       else
@@ -14,7 +12,14 @@ exports.testing = (req,res) =>{
         res.send('DB Connection Failed \n Error : ' + JSON.stringify(err,undefined, 2));
         myDBRequest = new con.Request();
       }
-    })
+    })*/
+
+    var sql = "CREATE TABLE user_table(id INT AUTO_INCREMENT PRIMARY KEY , name VARCHAR(255), email VARCHAR(255))";
+
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Table created");
+    });
 
     /*res.send({
         "code":res.statusCode,
@@ -23,4 +28,8 @@ exports.testing = (req,res) =>{
         status:1,
         "type":"SUCCESS"
       });*/
+}
+
+exports.insertdata = (req,res) =>{
+
 }
