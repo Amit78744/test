13ca0e72f2ffde380a88839dad0c57283236dd6f
ankitpansuria <ankit.pansuria@anviya.in>
2021-10-04 17:55:17 +0530

    changes done in plan module api.

--- app.js
+++ app.js
@@ -22,6 +22,6 @@ app.use(emailRoutes);
 
  ///////////////////////SQL Connection////////////////////////////////
   
-  app.listen(5000, function() {
-    console.log('Server is live on port ' + 5000);
+  app.listen(3000, function() {
+    console.log('Server is live on port ' + 3000);
   })
