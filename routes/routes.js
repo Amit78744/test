574fc5b671c7af359e588a501ebdc5bef05a4e81
ankitpansuria <ankit.pansuria@anviya.in>
2021-10-09 14:25:33 +0530

    Store Setup module, add store details, get store details and delete store details api completed.

--- routes/routes.js
+++ routes/routes.js
@@ -11,7 +11,7 @@ const plansController = require('../controller/plansController');
 ////test
 router.post('/test1', loginController.test1);
 router.post('/test2', loginController.test2);
-router.post('/getuser', loginController.getuser);
+router.get('/getuser', loginController.getuser);
 
 ////Register User
 router.post('/RegisterUser', signupController.registerUser);
