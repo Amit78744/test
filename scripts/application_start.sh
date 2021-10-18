070ac85da0f7cf3a622d451a064fc61c1e57c0a2
ankitpansuria <ankit.pansuria@anviya.in>
2021-10-18 08:16:39 +0530

    Rate and Reviews api completed.

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -12,6 +12,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
+npm i cors express nodemon
+npm install cors
 npm install
 npm start
 
