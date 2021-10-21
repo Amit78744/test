cb96430c8bcbc93403ccb828427f45df3e0440fc
ankitpansuria <ankit.pansuria@anviya.in>
2021-10-21 13:25:47 +0530

    updteuserProfile api changes done.

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,7 +3,7 @@
 #give permission for everything in the express-app directory
 sudo chmod -R 777 /home/ec2-user/Maidanlah
 
-#navigate into our working directory where we have all our github files
+#navigate into our working directory where we have all our github files 
 cd /home/ec2-user/Maidanlah
 
 #add npm and node to path
