0968b6ebd8e7592822a72bda186bda67bbbfb58a
ankitpansuria <ankit.pansuria@anviya.in>
2021-10-25 17:00:24 +0530

    Taxes and fees screen, Add taxes and update taxes api done.

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,7 +6,7 @@ sudo chmod -R 777 /home/ec2-user/Maidanlah
 #navigate into our working directory where we have all our github files 
 cd /home/ec2-user/Maidanlah
 
-nvm install 14.4.0
+#nvm install 14.4.0
 
 #install node modules
 npm i cors express nodemon
