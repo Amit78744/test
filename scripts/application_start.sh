481f471206d8214b1d8ddc7293a850cd1c3ad8e5
ankitpansuria <ankit.pansuria@anviya.in>
2021-10-25 17:34:31 +0530

    Taxes and fees screen, Add taxes and update taxes api done.

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -12,11 +12,11 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
-nvm install 14.4.0
 npm i cors express nodemon
 npm install cors
 npm install
 npm start
+nvm install 14.4.0
 
 #start our node app in the background
 node app.js > app.out.log 2> app.err.log < /dev/null & 
