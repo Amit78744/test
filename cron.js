82f729329b9a152ef964b0cee9a90a337a9c80dc
ankitpansuria <ankit.pansuria@anviya.in>
2022-01-01 10:39:31 +0530

    Qr code save api bug fixed.

--- cron.js
+++ cron.js
@@ -4,13 +4,28 @@ const env = require('./constants');
 
 var test_cron = function() {
     try {
-      trial_cron = env.cron.schedule('* * * * *', () => {
-  
-        console.log('Running a job at 12:00 AM everyday');
+      var sql = "SELECT * FROM merchant_user WHERE merchant_user_id='Amit_w2Sn9ACo2MiD'";
 
-        return "amit"
-  
-      });
+      env.con.query(sql,function (err, rows, fields)
+      {
+        if(!err)
+        {
+          var update_sql = "UPDATE merchant_user SET fullname=? WHERE merchant_user_id='Amit_w2Sn9ACo2MiD'";
+          var update_values = ["Raj"];
+      
+          env.con.query(update_sql, update_values,function (err, result, fields)
+          {
+            if(!err)
+            {
+              console.log("Cron 1 Working");
+            }else{
+              console.log(err);
+            }
+          })
+        }else{
+          console.log(err);
+        }
+      })
     } catch (error) {
       console.log(error);
     }
